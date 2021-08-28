ea6621daf7f96ee3ede79b086e448638baa8181b
Svetlanlka <Svetlanlka@users.noreply.github.com>
2021-08-28 22:34:31 +0300

    add jsdoc, rename 'n' on 'size'

--- source/chess.js
+++ source/chess.js
@@ -1,13 +1,19 @@
-'use strict'
+'use strict';
 
-const chess = (n) => {
-    if (!Number.isInteger(+n) || n <= 1) {
+/**
+  * Returns ASCII chessboard of size N * N characters from '*'
+  * 
+  * @param {string|number} size - chessboard size
+  * @returns {string}
+  */
+const chess = (size) => {
+    if (!Number.isInteger(+size) || size <= 1) {
         return null;
     }
 
     let chessBoard = '';
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             chessBoard += (i + j) % 2 == 0 ? '*' : ' ';
         }
         chessBoard += '\n';

80f453f703479f684dedf566b1950b0a5f053575
Svetlanlka <Svetlanlka@users.noreply.github.com>
2021-08-28 20:10:29 +0300

    remarks corrected

--- source/chess.js
+++ source/chess.js
@@ -1,15 +1,17 @@
 'use strict'
 
-function chess(n) {
-    if (!Number.isInteger(+n) || n <= 1) return null
-
-    let board = '';
-    for (var i = 0; i < n; i++) {
-        for (var j = 0; j < n; j++) {
-            board += (i + j) % 2 == 0 ? '*' : ' '
-        }
-        board += '\n';
+const chess = (n) => {
+    if (!Number.isInteger(+n) || n <= 1) {
+        return null;
     }
 
-    return board;
+    let chessBoard = '';
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            chessBoard += (i + j) % 2 == 0 ? '*' : ' ';
+        }
+        chessBoard += '\n';
+    }
+
+    return chessBoard;
 }
--- tests/chess.js
+++ tests/chess.js
@@ -3,12 +3,10 @@
 QUnit.module('Тестируем функцию chess', function () {
 	QUnit.test('Шахматной доски размерностью 0 не бывает', function (assert) {
 		assert.strictEqual(chess(0), null);
-		assert.strictEqual(chess('0'), null);
 	});
 	
 	QUnit.test('Шахматной доски 1 на 1 не бывает', function (assert) {
 		assert.strictEqual(chess(1), null);
-		assert.strictEqual(chess('1'), null);
 	});
 
 	QUnit.test('Некорректный тип данных параметра размерности шахматной доски', function (assert) {
@@ -22,12 +20,10 @@ QUnit.module('Тестируем функцию chess', function () {
 
 	QUnit.test('Параметр для размерности шахматной доски - не целое число', function (assert) {
 		assert.strictEqual(chess(1.23), null);
-		assert.strictEqual(chess('1.23'), null);
 	});
 
 	QUnit.test('Параметр для размерности шахматной доски не может быть отрицательным', function (assert) {
 		assert.strictEqual(chess(-1), null);
-		assert.strictEqual(chess('-1'), null);
 	});
 
 	QUnit.test('Шахматная доска 2 на 2', function (assert) {
@@ -35,6 +31,12 @@ QUnit.module('Тестируем функцию chess', function () {
 			'* \n' +
 			' *\n';
 		assert.strictEqual(chess(2), expected);
+	});
+
+	QUnit.test('Присутствует преобразование параметра-строки', function (assert) {
+		const expected =
+			'* \n' +
+			' *\n';
 		assert.strictEqual(chess('2'), expected);
 	});
 
@@ -44,7 +46,6 @@ QUnit.module('Тестируем функцию chess', function () {
 			' * \n' +
 			'* *\n';
 		assert.strictEqual(chess(3), expected);
-		assert.strictEqual(chess('3'), expected);
 	});
 
 	QUnit.test('Шахматная доска 4 на 4', function (assert) {
@@ -54,7 +55,6 @@ QUnit.module('Тестируем функцию chess', function () {
 			'* * \n' +
 			' * *\n';
 		assert.strictEqual(chess(4), expected);
-		assert.strictEqual(chess('4'), expected);
 	});
 
 	QUnit.test('Шахматная доска 8 на 8', function (assert) {
@@ -68,7 +68,6 @@ QUnit.module('Тестируем функцию chess', function () {
 			'* * * * \n' +
 			' * * * *\n';
 		assert.strictEqual(chess(8), expected);
-		assert.strictEqual(chess('8'), expected);
 	});
 
 });
